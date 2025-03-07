1c51625a70616c444b4ec5c9a84b32736fd90aa7
Lamont Crook <lamont@adobe.com>
2025-03-06 21:27:37 -0700

    update context.js

--- src/utils/context.js
+++ src/utils/context.js
@@ -2,8 +2,8 @@ import { createContext } from 'react';
 import BrokenImage from '../media/broken.jpg';
 
 const defaultEndpoint = '/content/_cq_graphql/aem-demo-assets/endpoint.json';
-const defaultProject = 'wknd-headless';
-const defaultServiceURL = 'https://publish-p124331-e1227315.adobeaemcloud.com/';
+const defaultProject = 'aem-headless-wknd';
+const defaultServiceURL = 'https://author-p124331-e1227315.adobeaemcloud.com/';
 const defaultPlaceholdersExtensionURL = 'https://1154643-geoipplaceholders.adobeio-static.net/api/v1/web/geoip-placeholders';
 
 export const AppContext = createContext({
